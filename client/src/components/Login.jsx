90096fe870a1626b6c7b99875d0594ec04992ceb
Waldemar Kunz <w_kunz@web.de>
2020-09-25 15:25:10 +0200

    refactoring styles

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -20,59 +20,64 @@ export default function LoginForm() {
   }
 
   return (
-    <div className="login">
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item>
-          <h1>Login</h1>
-        </Form.Item>
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              type: 'email',
-              required: true,
-              message: 'Please input your Email',
-            },
-          ]}
-        >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Email"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: 'Please input your Password!' }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            {/* <Checkbox>Remember me</Checkbox> */}
-          </Form.Item>
-        </Form.Item>
-
-        <Form.Item>
-          <Button
-            style={{ marginBottom: '10px' }}
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
+    <div className="background-image">
+      <div className="modal-background">
+        <div className="login-form">
+          <Form
+            name="normal_login"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
           >
-            Log in
-          </Button>
-          Or <a href="/signup">sign up!</a>
-        </Form.Item>
-      </Form>
+            <Form.Item>
+              <h1>Login</h1>
+            </Form.Item>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  type: 'email',
+                  required: true,
+                  message: 'Please input your Email',
+                },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Email"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: 'Please input your Password!' },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                {/* <Checkbox>Remember me</Checkbox> */}
+              </Form.Item>
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                style={{ marginBottom: '10px' }}
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Log in
+              </Button>
+              Or <a href="/signup">sign up!</a>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     </div>
   )
 }
